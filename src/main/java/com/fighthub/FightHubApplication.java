ddb0ae975a11f5175eb157b1f324283c29a501bc
eduardo-toste <duciochetti@gmail.com>
2025-08-28 20:38:19 -0300

    initial project

---
+++ src/main/java/com/fighthub/FightHubApplication.java
@@ -0,0 +1,13 @@
+package com.fighthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fighthub/FightHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fighthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
