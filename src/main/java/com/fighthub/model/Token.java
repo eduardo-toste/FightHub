b121f4b191aa0c322f9fc724a9f52d5626919059
eduardo-toste <duciochetti@gmail.com>
2025-08-30 02:08:00 -0300

    feat: create token entity

---
+++ src/main/java/com/fighthub/model/Token.java
@@ -0,0 +1,38 @@
+package com.fighthub.model;
+
+import com.fighthub.model.enums.TokenType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TokenType tokenType;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    @Column(nullable = false)
+    private boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
