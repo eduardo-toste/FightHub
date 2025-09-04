a136fd184ceb57ff8ece168532ca1a3c5040f143
eduardo-toste <duciochetti@gmail.com>
2025-09-03 22:15:06 -0300

    feat: create responsavel entity

---
+++ src/main/java/com/fighthub/model/Responsavel.java
@@ -0,0 +1,30 @@
+package com.fighthub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "responsaveis")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Responsavel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToMany(mappedBy = "responsaveis")
+    private List<Aluno> alunos = new ArrayList<>();
+
+}
