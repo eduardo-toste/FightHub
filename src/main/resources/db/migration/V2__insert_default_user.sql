5f4be8d0455e34fde4aa5615846e6b0c0f92d3f3
eduardo-toste <duciochetti@gmail.com>
2025-09-03 22:17:58 -0300

    feat: create insert default user migration

---
+++ src/main/resources/db/migration/V2__insert_default_user.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuarios (id, nome, email, senha, role, login_social, ativo)
+VALUES (
+    '00000000-0000-0000-0000-000000000001',
+    'Administrador',
+    'admin@fighthub.com',
+    '$2a$12$j8YVgfczwSJn67JYPFULB.P2MUdlQ0mGhN34UaNUmrgGSTWtYtT.S', -- senha: admin123
+    'ADMIN',
+    false,
+    true
+);
