ea812a82ad006fb95fdfcfb4cf0b201b604ac928
eduardo-toste <duciochetti@gmail.com>
2025-09-10 22:21:15 -0300

    feat: create method to find revoked access tokens

--- src/main/java/com/fighthub/repository/TokenRepository.java
+++ src/main/java/com/fighthub/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package com.fighthub.repository;
 
 import com.fighthub.model.Token;
 import com.fighthub.model.Usuario;
+import com.fighthub.model.enums.TokenType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.UUID;
 public interface TokenRepository extends JpaRepository<Token, UUID> {
 
     List<Token> findAllByUsuarioAndExpiredFalseAndRevokedFalse(Usuario usuario);
+
+    List<Token> findAllByUsuarioAndRevokedFalseAndTokenType(Usuario usuario, TokenType tokenType);
 
     Optional<Token> findByToken(String jwt);
 
