bc71bd89c361e4324820897b44e633b1775cc9cb
eduardo-toste <duciochetti@gmail.com>
2025-09-14 20:24:58 -0300

    feat: create user controller base class

---
+++ src/main/java/com/fighthub/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.fighthub.controller;
+
+import com.fighthub.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+}
