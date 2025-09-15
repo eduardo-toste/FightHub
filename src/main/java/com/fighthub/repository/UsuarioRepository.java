85cc814671f7925d31349378e01ef10d8ad694e9
eduardo-toste <duciochetti@gmail.com>
2025-09-15 07:54:25 -0300

    feat: create exists by email method

--- src/main/java/com/fighthub/repository/UsuarioRepository.java
+++ src/main/java/com/fighthub/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.fighthub.repository;
 
 import com.fighthub.model.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +12,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 }
