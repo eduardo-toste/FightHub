8e698986e636a6ca142346c0cdfef079918cf8e7
eduardo-toste <duciochetti@gmail.com>
2025-09-15 07:54:52 -0300

    feat: create new business exception, named ValidacaoException

---
+++ src/main/java/com/fighthub/exception/ValidacaoException.java
@@ -0,0 +1,9 @@
+package com.fighthub.exception;
+
+public class ValidacaoException extends BusinessException {
+
+    public ValidacaoException(String message) {
+        super(message);
+    }
+
+}
