6ea93907a7a605b03c326d5fb97be08f6b6c32f3
eduardo-toste <duciochetti@gmail.com>
2025-09-16 08:04:04 -0300

    feat: create email configuration

---
+++ src/main/java/com/fighthub/config/EmailConfig.java
@@ -0,0 +1,44 @@
+package com.fighthub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.tls-enabled}")
+    private boolean tlsEnabled;
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(host);
+        sender.setPort(port);
+        sender.setUsername(username);
+        sender.setPassword(password);
+
+        Properties props = sender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", String.valueOf(tlsEnabled));
+
+        return sender;
+    }
+
+}
