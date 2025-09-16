a74e73bdc2b810660216e8c5d3aaae52c478e962
eduardo-toste <duciochetti@gmail.com>
2025-09-16 19:27:12 -0300

    feat: create activate account endpoint

---
+++ src/main/java/com/fighthub/controller/AtivacaoController.java
@@ -0,0 +1,26 @@
+package com.fighthub.controller;
+
+import com.fighthub.dto.AtivacaoRequest;
+import com.fighthub.service.AtivacaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ativar")
+@RequiredArgsConstructor
+public class AtivacaoController {
+
+    private final AtivacaoService ativacaoService;
+
+    @PostMapping
+    public ResponseEntity<Void> ativarConta(@RequestBody AtivacaoRequest request) {
+        ativacaoService.ativarConta(request.token(), request.senha());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
