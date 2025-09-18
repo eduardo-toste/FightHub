878ca8138bdbe69c2ac756d2364871a841ceeb82
eduardo-toste <duciochetti@gmail.com>
2025-09-18 10:54:14 -0300

    feat: create address request dto

---
+++ src/main/java/com/fighthub/dto/EnderecoRequest.java
@@ -0,0 +1,30 @@
+package com.fighthub.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EnderecoRequest(
+
+        @NotBlank @Size(max = 9)
+        String cep,
+
+        @NotBlank @Size(max = 100)
+        String logradouro,
+
+        @NotBlank @Size(max = 10)
+        String numero,
+
+        @Size(max = 50)
+        String complemento,
+
+        @NotBlank @Size(max = 50)
+        String bairro,
+
+        @NotBlank @Size(max = 50)
+        String cidade,
+
+        @NotBlank @Size(min = 2, max = 2)
+        String estado
+
+) {
+}
