acc2d06415df693f82e9b02ec9f82fe474a892f2
eduardo-toste <duciochetti@gmail.com>
2025-09-18 21:32:55 -0300

    feat: create get and get by id endpoints

--- src/main/java/com/fighthub/controller/AlunoController.java
+++ src/main/java/com/fighthub/controller/AlunoController.java
@@ -1,16 +1,20 @@
 package com.fighthub.controller;
 
-import com.fighthub.dto.CriarAlunoRequest;
+import com.fighthub.dto.aluno.AlunoDetalhadoResponse;
+import com.fighthub.dto.aluno.AlunoResponse;
+import com.fighthub.dto.aluno.CriarAlunoRequest;
 import com.fighthub.service.AlunoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/alunos")
@@ -26,4 +30,15 @@ public class AlunoController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping
+    public ResponseEntity<Page<AlunoResponse>> obterAlunos(Pageable pageable) {
+        Page<AlunoResponse> alunos = alunoService.obterTodos(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(alunos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AlunoDetalhadoResponse> obterAluno(@PathVariable UUID id) {
+        var aluno = alunoService.obterAluno(id);
+        return ResponseEntity.status(HttpStatus.OK).body(aluno);
+    }
 }
