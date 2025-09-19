246b513a06a57c68f219bde12d2a389f2cba6aa4
eduardo-toste <duciochetti@gmail.com>
2025-09-18 21:26:28 -0300

    feat: create student response dto

---
+++ src/main/java/com/fighthub/dto/AlunoResponse.java
@@ -0,0 +1,22 @@
+package com.fighthub.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record AlunoResponse(
+        UUID id,
+        String nome,
+        String email,
+        String foto,
+        boolean ativo,
+        LocalDate dataNascimento,
+        LocalDate dataMatricula,
+        List<ResponsavelResumo> responsaveis
+) {
+    public record ResponsavelResumo(
+            UUID id,
+            String nome,
+            String email
+    ) {}
+}
