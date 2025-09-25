00ad7d84a6e771875d5ccf7e726056b5a76dfe25
eduardo-toste <duciochetti@gmail.com>
2025-09-25 07:49:06 -0300

    feat: create attribute and method to use validation errors list

--- src/main/java/com/fighthub/exception/dto/ErrorResponse.java
+++ src/main/java/com/fighthub/exception/dto/ErrorResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.http.HttpStatus;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 public record ErrorResponse(
         OffsetDateTime timestamp,
@@ -11,7 +12,9 @@ public record ErrorResponse(
         String error,
         String message,
         @JsonInclude(JsonInclude.Include.NON_NULL)
-        String path
+        String path,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        List<String> validationError
 ) {
     public static ErrorResponse from(HttpStatus status, String message, String path) {
         return new ErrorResponse(
@@ -19,7 +22,19 @@ public record ErrorResponse(
                 status.value(),
                 status.getReasonPhrase(),
                 message,
-                path
+                path,
+                null
+        );
+    }
+
+    public static ErrorResponse from(HttpStatus status, String message, String path, List<String> validationError) {
+        return new ErrorResponse(
+                OffsetDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                message,
+                path,
+                validationError
         );
     }
 }
