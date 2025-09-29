b1af4aed6dc8e9818cf7b8e3c53ffad53a0facd8
eduardo-toste <duciochetti@gmail.com>
2025-09-29 13:38:30 -0300

    feat: create get user endpoints

--- src/main/java/com/fighthub/controller/UsuarioController.java
+++ src/main/java/com/fighthub/controller/UsuarioController.java
@@ -1,9 +1,21 @@
 package com.fighthub.controller;
 
+import com.fighthub.dto.usuario.UsuarioDetalhadoResponse;
+import com.fighthub.dto.usuario.UsuarioResponse;
+import com.fighthub.model.Usuario;
 import com.fighthub.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -11,5 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<UsuarioResponse>> obterUsuarios(Pageable pageable) {
+        var usuarios = usuarioService.obterTodosUsuarios(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UsuarioDetalhadoResponse> obterUsuarioEspecifico(@PathVariable UUID id) {
+        var usuario = usuarioService.obterUsuario(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuario);
+    }
 
 }
