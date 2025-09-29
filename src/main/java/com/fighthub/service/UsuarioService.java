033730582a2f05e8796216894de1ee6402fc859e
eduardo-toste <duciochetti@gmail.com>
2025-09-29 13:38:40 -0300

    feat: create get user methods

--- src/main/java/com/fighthub/service/UsuarioService.java
+++ src/main/java/com/fighthub/service/UsuarioService.java
@@ -1,8 +1,16 @@
 package com.fighthub.service;
 
+import com.fighthub.dto.usuario.UsuarioDetalhadoResponse;
+import com.fighthub.dto.usuario.UsuarioResponse;
+import com.fighthub.exception.UsuarioNaoEncontradoException;
+import com.fighthub.mapper.UsuarioMapper;
 import com.fighthub.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +18,12 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    public Page<UsuarioResponse> obterTodosUsuarios(Pageable pageable) {
+        return UsuarioMapper.toPage(usuarioRepository.findAll(pageable));
+    }
+
+    public UsuarioDetalhadoResponse obterUsuario(UUID id) {
+        return UsuarioMapper.toDetailedDTO(usuarioRepository.findById(id)
+                .orElseThrow(UsuarioNaoEncontradoException::new));
+    }
 }
