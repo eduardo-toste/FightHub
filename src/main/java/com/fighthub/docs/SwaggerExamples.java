3baf6cadd9ececf137b0bb9e86f84c7a0164a77b
eduardo-toste <duciochetti@gmail.com>
2025-10-09 21:23:21 -0300

    feat: create invalid token swagger example

--- src/main/java/com/fighthub/docs/SwaggerExamples.java
+++ src/main/java/com/fighthub/docs/SwaggerExamples.java
@@ -15,6 +15,16 @@ public class SwaggerExamples {
         }
     """;
 
+    public static final String TOKEN_INVALIDO = """
+        {
+          "timestamp": "2025-09-09T17:49:11.68509-03:00",
+          "status": 401,
+          "error": "Unauthorized",
+          "message": "Token inválido ou malformado.",
+          "path": "/auth/refresh"
+        }
+    """;
+
     public static final String TOKEN_EXPIRADO = """
         {
           "timestamp": "2025-09-09T17:49:27.511747-03:00",
