8483468dd8e1db98ca48364963d08f673a18a352
eduardo-toste <duciochetti@gmail.com>
2025-10-11 18:34:03 -0300

    feat: create get responsible endpoints

--- src/main/java/com/fighthub/controller/ResponsavelController.java
+++ src/main/java/com/fighthub/controller/ResponsavelController.java
@@ -1,17 +1,20 @@
 package com.fighthub.controller;
 
 import com.fighthub.dto.responsavel.CriarResponsavelRequest;
+import com.fighthub.dto.responsavel.ResponsavelDetalhadoResponse;
+import com.fighthub.dto.responsavel.ResponsavelResponse;
 import com.fighthub.service.ResponsavelService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/responsaveis")
@@ -26,6 +29,20 @@ public class ResponsavelController {
     public ResponseEntity<Void> criarResponsavel(@RequestBody @Valid CriarResponsavelRequest request) {
         responsavelService.criacaoResponsavel(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<Page<ResponsavelResponse>> obterResponsaveis(Pageable pageable) {
+        var responsaveis = responsavelService.obterTodosResponsaveis(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(responsaveis);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<ResponsavelDetalhadoResponse> obterResponsavel(@PathVariable UUID id) {
+        var responsavel = responsavelService.obterResponsavelPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(responsavel);
     }
 
 }
