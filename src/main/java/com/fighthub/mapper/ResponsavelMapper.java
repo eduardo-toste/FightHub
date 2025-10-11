9444d1a1b54fa15bc35f8de4585ebd1a8803b9f5
eduardo-toste <duciochetti@gmail.com>
2025-10-11 18:34:52 -0300

    feat: create responsible mapper

---
+++ src/main/java/com/fighthub/mapper/ResponsavelMapper.java
@@ -0,0 +1,37 @@
+package com.fighthub.mapper;
+
+import com.fighthub.dto.endereco.EnderecoResponse;
+import com.fighthub.dto.responsavel.ResponsavelDetalhadoResponse;
+import com.fighthub.dto.responsavel.ResponsavelResponse;
+import com.fighthub.model.Responsavel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+public class ResponsavelMapper {
+
+        public static ResponsavelResponse toDTO(Responsavel responsavel) {
+            return new ResponsavelResponse(
+                    responsavel.getId(),
+                    responsavel.getUsuario().getNome(),
+                    responsavel.getUsuario().getEmail(),
+                    responsavel.getUsuario().getTelefone(),
+                    responsavel.getUsuario().getFoto()
+            );
+        }
+
+        public static ResponsavelDetalhadoResponse toDetailedDTO(Responsavel responsavel) {
+            return new ResponsavelDetalhadoResponse(
+                    responsavel.getId(),
+                    responsavel.getUsuario().getNome(),
+                    responsavel.getUsuario().getEmail(),
+                    responsavel.getUsuario().getTelefone(),
+                    responsavel.getUsuario().getFoto(),
+                    EnderecoResponse.fromEntity(responsavel.getUsuario().getEndereco())
+            );
+        }
+
+        public static Page<ResponsavelResponse> toPageDTO(Page<Responsavel> page) {
+            return page.map(ResponsavelMapper::toDTO);
+        }
+
+}
