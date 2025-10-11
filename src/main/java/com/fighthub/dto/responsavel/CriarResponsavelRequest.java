0a3a127880cf53ecaf0b21f2fa15d7b4f330e73a
eduardo-toste <duciochetti@gmail.com>
2025-10-11 17:38:05 -0300

    feat: create responsible creation request dto

---
+++ src/main/java/com/fighthub/dto/responsavel/CriarResponsavelRequest.java
@@ -0,0 +1,26 @@
+package com.fighthub.dto.responsavel;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record CriarResponsavelRequest(
+
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "E-mail é obrigatório")
+        @Email(message = "E-mail deve ser válido")
+        String email,
+
+        @CPF(message = "CPF deve ser válido")
+        @NotBlank(message = "CPF é obrigatório")
+        String cpf
+
+) {
+}
