08143cd5c1a03ca8fd37372ea3f12308e20111cf
eduardo-toste <duciochetti@gmail.com>
2025-10-13 18:48:59 -0300

    feat: create service methods to link and unlink student to responsible

--- src/main/java/com/fighthub/service/ResponsavelService.java
+++ src/main/java/com/fighthub/service/ResponsavelService.java
@@ -3,20 +3,25 @@ package com.fighthub.service;
 import com.fighthub.dto.responsavel.CriarResponsavelRequest;
 import com.fighthub.dto.responsavel.ResponsavelDetalhadoResponse;
 import com.fighthub.dto.responsavel.ResponsavelResponse;
+import com.fighthub.exception.AlunoNaoEncontradoException;
 import com.fighthub.exception.CpfExistenteException;
 import com.fighthub.exception.ResponsavelNaoEncontradoException;
 import com.fighthub.exception.ValidacaoException;
 import com.fighthub.mapper.ResponsavelMapper;
+import com.fighthub.model.Aluno;
 import com.fighthub.model.Responsavel;
 import com.fighthub.model.Usuario;
 import com.fighthub.model.enums.Role;
+import com.fighthub.repository.AlunoRepository;
 import com.fighthub.repository.ResponsavelRepository;
 import com.fighthub.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,9 +30,11 @@ public class ResponsavelService {
 
     private final ResponsavelRepository responsavelRepository;
     private final UsuarioRepository usuarioRepository;
+    private final AlunoRepository alunoRepository;
     private final TokenService tokenService;
     private final EmailService emailService;
 
+    @Transactional
     public void criacaoResponsavel(CriarResponsavelRequest request) {
         if (usuarioRepository.existsByEmail(request.email())) {
             throw new ValidacaoException("E-mail já cadastrado");
@@ -61,5 +68,33 @@ public class ResponsavelService {
     public ResponsavelDetalhadoResponse obterResponsavelPorId(UUID id) {
         return ResponsavelMapper.toDetailedDTO(responsavelRepository.findById(id)
                 .orElseThrow(ResponsavelNaoEncontradoException::new));
+    }
+
+    @Transactional
+    public void vincularAlunoAoResponsavel(UUID idResponsavel, UUID idAluno) {
+        var responsavel = responsavelRepository.findById(idResponsavel)
+                .orElseThrow(ResponsavelNaoEncontradoException::new);
+
+        var aluno = alunoRepository.findById(idAluno)
+                .orElseThrow(AlunoNaoEncontradoException::new);
+
+        if (responsavel.getAlunos().contains(aluno)) throw new ValidacaoException("Vínculo de responsabilidade já estabelecido.");
+
+        responsavel.getAlunos().add(aluno);
+        responsavelRepository.save(responsavel);
+    }
+
+    @Transactional
+    public void removerVinculoAlunoEResponsavel(UUID idResponsavel, UUID idAluno) {
+        var responsavel = responsavelRepository.findById(idResponsavel)
+                .orElseThrow(ResponsavelNaoEncontradoException::new);
+
+        var aluno = alunoRepository.findById(idAluno)
+                .orElseThrow(AlunoNaoEncontradoException::new);
+
+        if (!responsavel.getAlunos().contains(aluno)) throw new ValidacaoException("Responsável não vinculado ao aluno.");
+
+        responsavel.getAlunos().remove(aluno);
+        responsavelRepository.save(responsavel);
     }
 }
