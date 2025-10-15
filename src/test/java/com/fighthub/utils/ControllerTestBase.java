ff363e0bcd7f11e9e98c9b033c3f03aafa6955c9
eduardo-toste <duciochetti@gmail.com>
2025-10-14 21:15:32 -0300

    feat: create abstract controller test base class

---
+++ src/test/java/com/fighthub/utils/ControllerTestBase.java
@@ -0,0 +1,26 @@
+package com.fighthub.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fighthub.config.TestSecurityConfig;
+import com.fighthub.repository.TokenRepository;
+import com.fighthub.repository.UsuarioRepository;
+import com.fighthub.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+@Import(TestSecurityConfig.class)
+public abstract class ControllerTestBase {
+
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected ObjectMapper objectMapper;
+
+    @MockBean protected JwtService jwtService;
+    @MockBean protected TokenRepository tokenRepository;
+    @MockBean protected UsuarioRepository usuarioRepository;
+    @MockBean protected ErrorWriter errorWriter;
+
+}
