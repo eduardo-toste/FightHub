10d6eddf2c99d96ca94c9bcaa18fba858d386e46
eduardo-toste <duciochetti@gmail.com>
2025-10-15 19:06:26 -0300

    feat: create integration test base class

---
+++ src/test/java/com/fighthub/utils/IntegrationTestBase.java
@@ -0,0 +1,20 @@
+package com.fighthub.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fighthub.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public abstract class IntegrationTestBase {
+
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected ObjectMapper objectMapper;
+    @Autowired protected UsuarioRepository usuarioRepository;
+
+}
