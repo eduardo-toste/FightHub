926f0700d986f0f1cf0a57ad8d40b7aa2942dbd2
eduardo-toste <duciochetti@gmail.com>
2025-10-26 15:27:05 -0300

    feat: create teacher module queries

--- src/main/java/com/fighthub/repository/ProfessorRepository.java
+++ src/main/java/com/fighthub/repository/ProfessorRepository.java
@@ -1,9 +1,22 @@
 package com.fighthub.repository;
 
 import com.fighthub.model.Professor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
+
+    @Override
+    @EntityGraph(attributePaths = {"usuario"})
+    Page<Professor> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"usuario"})
+    Optional<Professor> findById(UUID id);
+
 }
