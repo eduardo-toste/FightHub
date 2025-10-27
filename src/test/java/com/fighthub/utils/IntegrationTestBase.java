b005d3d9a1c0b96d7c6de70f21e279546cb9e679
eduardo-toste <duciochetti@gmail.com>
2025-10-27 15:48:24 -0300

    refactor: changing port used in integration tests to a mocked port

--- src/test/java/com/fighthub/utils/IntegrationTestBase.java
+++ src/test/java/com/fighthub/utils/IntegrationTestBase.java
@@ -16,7 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import jakarta.transaction.Transactional;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
