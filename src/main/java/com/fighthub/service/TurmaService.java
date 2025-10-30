72133ed27ae8f96bcaa6833e25dbdbcc6fe14c63
eduardo-toste <duciochetti@gmail.com>
2025-10-30 13:56:43 -0300

    feat: create put, patch and delete service methods

--- src/main/java/com/fighthub/service/TurmaService.java
+++ src/main/java/com/fighthub/service/TurmaService.java
@@ -2,10 +2,13 @@ package com.fighthub.service;
 
 import com.fighthub.dto.turma.TurmaRequest;
 import com.fighthub.dto.turma.TurmaResponse;
+import com.fighthub.dto.turma.TurmaUpdateCompletoRequest;
+import com.fighthub.dto.turma.TurmaUpdateStatusRequest;
 import com.fighthub.exception.ProfessorNaoEncontradoException;
 import com.fighthub.exception.TurmaNaoEncontradaException;
 import com.fighthub.mapper.TurmaMapper;
 import com.fighthub.model.Professor;
+import com.fighthub.model.Turma;
 import com.fighthub.repository.ProfessorRepository;
 import com.fighthub.repository.TurmaRepository;
 import lombok.RequiredArgsConstructor;
@@ -35,6 +38,30 @@ public class TurmaService {
 
     public TurmaResponse buscarTurmaPorId(UUID id) {
         return TurmaMapper.toDTO(turmaRepository.findById(id)
+                .orElseThrow(TurmaNaoEncontradaException::new));
+    }
+
+    public TurmaResponse atualizarTurma(UUID id, TurmaUpdateCompletoRequest request) {
+        Turma turma = turmaRepository.findById(id)
+                .orElseThrow(TurmaNaoEncontradaException::new);
+
+        Professor professor = professorRepository.findById(request.professorId())
+                .orElseThrow(ProfessorNaoEncontradoException::new);
+
+        turma.putUpdate(request, professor);
+        return TurmaMapper.toDTO(turmaRepository.save(turma));
+    }
+
+    public TurmaResponse atualizarStatusTurma(UUID id, TurmaUpdateStatusRequest request) {
+        Turma turma = turmaRepository.findById(id)
+                .orElseThrow(TurmaNaoEncontradaException::new);
+
+        turma.setAtivo(request.ativo());
+        return TurmaMapper.toDTO(turmaRepository.save(turma));
+    }
+
+    public void excluirTurma(UUID id) {
+        turmaRepository.delete(turmaRepository.findById(id)
                 .orElseThrow(TurmaNaoEncontradaException::new));
     }
 }
