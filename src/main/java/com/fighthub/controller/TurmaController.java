666a7918f5e3af8b6625269265dfa319f80de82f
eduardo-toste <duciochetti@gmail.com>
2025-10-30 13:55:35 -0300

    feat: create put, patch and delete endpoints

--- src/main/java/com/fighthub/controller/TurmaController.java
+++ src/main/java/com/fighthub/controller/TurmaController.java
@@ -2,6 +2,8 @@ package com.fighthub.controller;
 
 import com.fighthub.dto.turma.TurmaRequest;
 import com.fighthub.dto.turma.TurmaResponse;
+import com.fighthub.dto.turma.TurmaUpdateCompletoRequest;
+import com.fighthub.dto.turma.TurmaUpdateStatusRequest;
 import com.fighthub.service.TurmaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,16 +31,35 @@ public class TurmaController {
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', ' PROFESSOR')")
     public ResponseEntity<Page<TurmaResponse>> buscarTurmas(Pageable pageable) {
         var turmas = turmaService.buscarTurmas(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(turmas);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', ' PROFESSOR')")
     public ResponseEntity<TurmaResponse> buscarTurmaPorId(@PathVariable UUID id) {
         var turma = turmaService.buscarTurmaPorId(id);
         return ResponseEntity.status(HttpStatus.OK).body(turma);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', ' PROFESSOR')")
+    public ResponseEntity<TurmaResponse> atualizarTurma(@PathVariable UUID id, @RequestBody @Valid TurmaUpdateCompletoRequest request) {
+        var turmaAtualizada = turmaService.atualizarTurma(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(turmaAtualizada);
+    }
+
+    @PatchMapping("/{id}/status")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', ' PROFESSOR')")
+    public ResponseEntity<TurmaResponse> atualizarStatusTurma(@PathVariable UUID id, @RequestBody @Valid TurmaUpdateStatusRequest request) {
+        var turmaAtualizada = turmaService.atualizarStatusTurma(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(turmaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', ' PROFESSOR')")
+    public ResponseEntity<Void> excluirTurma(@PathVariable UUID id) {
+        turmaService.excluirTurma(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
