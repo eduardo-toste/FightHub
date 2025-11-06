c1c68b23f6af56d3084841a2cb64d6c4e346acaf
eduardo-toste <duciochetti@gmail.com>
2025-11-06 11:41:40 -0300

    feat: create to dto and to page dto mappers

--- src/main/java/com/fighthub/mapper/AulaMapper.java
+++ src/main/java/com/fighthub/mapper/AulaMapper.java
@@ -1,8 +1,10 @@
 package com.fighthub.mapper;
 
 import com.fighthub.dto.aula.AulaRequest;
+import com.fighthub.dto.aula.AulaResponse;
 import com.fighthub.model.Aula;
 import com.fighthub.model.Turma;
+import org.springframework.data.domain.Page;
 
 public class AulaMapper {
 
@@ -14,6 +16,22 @@ public class AulaMapper {
               turma,
               request.limiteAlunos()
         );
+    }
+
+    public static AulaResponse toDTO(Aula aula) {
+        return new AulaResponse(
+                aula.getId(),
+                aula.getTitulo(),
+                aula.getDescricao(),
+                aula.getData(),
+                aula.getTurma().getId(),
+                aula.getLimiteAlunos(),
+                aula.isAtivo()
+        );
+    }
+
+    public static Page<AulaResponse> toPageDTO(Page<Aula> page) {
+        return page.map(AulaMapper::toDTO);
     }
 
 }
