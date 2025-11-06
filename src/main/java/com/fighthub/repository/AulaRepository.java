1352bbf20fefc9a97e4be2f07059e46970fb31aa
eduardo-toste <duciochetti@gmail.com>
2025-11-06 13:51:33 -0300

    feat: create custom find queries

--- src/main/java/com/fighthub/repository/AulaRepository.java
+++ src/main/java/com/fighthub/repository/AulaRepository.java
@@ -1,9 +1,16 @@
 package com.fighthub.repository;
 
 import com.fighthub.model.Aula;
+import com.fighthub.model.Turma;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AulaRepository extends JpaRepository<Aula, UUID> {
+
+    Page<Aula> findByTurmaIn(List<Turma> turmas, Pageable pageable);
+
 }
