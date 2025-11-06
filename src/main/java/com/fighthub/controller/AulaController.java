6647185714ddc1a011eb89579258485638e754a8
eduardo-toste <duciochetti@gmail.com>
2025-11-05 21:38:56 -0300

    feat: create class endpoints

--- src/main/java/com/fighthub/controller/AulaController.java
+++ src/main/java/com/fighthub/controller/AulaController.java
@@ -1,9 +1,16 @@
 package com.fighthub.controller;
 
+import com.fighthub.dto.aula.AulaRequest;
+import com.fighthub.dto.aula.AulaResponse;
 import com.fighthub.service.AulaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/aulas")
@@ -11,5 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AulaController {
 
     private final AulaService aulaService;
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<AulaResponse> criarAula(@RequestBody @Valid AulaRequest request) {
+        aulaService.criarAula(request);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PatchMapping("/{idAula}/turmas/{idTurma}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<Void> vincularTurma(@PathVariable UUID idAula, @PathVariable UUID idTurma) {
+        aulaService.vincularTurma(idAula, idTurma);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{idAula}/turmas/{idTurma}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<Void> desvincularTurma(@PathVariable UUID idAula, @PathVariable UUID idTurma) {
+        aulaService.desvincularTurma(idAula, idTurma);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
 }
