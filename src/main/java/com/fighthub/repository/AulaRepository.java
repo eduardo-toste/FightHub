90be1d83ef8cf28289859e3b4d294f6fa670de2d
eduardo-toste <duciochetti@gmail.com>
2025-11-10 07:42:25 -0300

    feat: update query to find by status too

--- src/main/java/com/fighthub/repository/AulaRepository.java
+++ src/main/java/com/fighthub/repository/AulaRepository.java
@@ -2,6 +2,7 @@ package com.fighthub.repository;
 
 import com.fighthub.model.Aula;
 import com.fighthub.model.Turma;
+import com.fighthub.model.enums.ClassStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,6 @@ import java.util.UUID;
 
 public interface AulaRepository extends JpaRepository<Aula, UUID> {
 
-    Page<Aula> findByTurmaIn(List<Turma> turmas, Pageable pageable);
+    Page<Aula> findByStatusAndTurmaIn(ClassStatus status, List<Turma> turmas, Pageable pageable);
 
 }
