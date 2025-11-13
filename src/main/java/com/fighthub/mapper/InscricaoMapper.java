cbefa7cb8f457d2290e41ce401ffd540e5fecd5e
eduardo-toste <duciochetti@gmail.com>
2025-11-13 15:46:05 -0300

    feat: create subscription mapper

---
+++ src/main/java/com/fighthub/mapper/InscricaoMapper.java
@@ -0,0 +1,23 @@
+package com.fighthub.mapper;
+
+import com.fighthub.dto.inscricao.InscricaoResponse;
+import com.fighthub.model.Inscricao;
+import org.springframework.data.domain.Page;
+
+public class InscricaoMapper {
+
+    public static InscricaoResponse toDTO(Inscricao inscricao) {
+        return new InscricaoResponse(
+                inscricao.getId(),
+                inscricao.getAluno().getId(),
+                inscricao.getAula().getId(),
+                inscricao.getStatus(),
+                inscricao.getDataInscricao()
+        );
+    }
+
+    public static Page<InscricaoResponse> toPageDTO(Page<Inscricao> page) {
+        return page.map(InscricaoMapper::toDTO);
+    }
+
+}
