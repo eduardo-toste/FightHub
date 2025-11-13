6db759fdfbfe76db3c50d25b8eb51ec0b9fa54e9
eduardo-toste <duciochetti@gmail.com>
2025-11-13 15:46:16 -0300

    feat: create subscription get endpoints

--- src/main/java/com/fighthub/controller/InscricaoController.java
+++ src/main/java/com/fighthub/controller/InscricaoController.java
@@ -1,8 +1,11 @@
 package com.fighthub.controller;
 
+import com.fighthub.dto.inscricao.InscricaoResponse;
 import com.fighthub.service.InscricaoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,5 +31,19 @@ public class InscricaoController {
     public ResponseEntity<Void> cancelarInscricaoAluno(@PathVariable UUID idAula, HttpServletRequest request) {
         inscricaoService.cancelarInscricao(idAula, request);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/aulas/{idAula}/inscricoes")
+    @PreAuthorize("hasRole('ADMIN', 'COORDENADOR', 'PROFESSOR')")
+    public ResponseEntity<Page<InscricaoResponse>> buscarInscricoes(@PathVariable UUID idAula, Pageable pageable) {
+        var inscricoes = inscricaoService.buscarInscricoesPorAula(idAula, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(inscricoes);
+    }
+
+    @DeleteMapping("/minhas-inscricoes")
+    @PreAuthorize("hasRole('ALUNO')")
+    public ResponseEntity<Page<InscricaoResponse>> buscarInscricoesProprias(HttpServletRequest request,  Pageable pageable) {
+        var inscricoes = inscricaoService.buscarInscricoesProprias(request, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(inscricoes);
     }
 }
