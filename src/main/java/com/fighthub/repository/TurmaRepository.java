06fcfbe3d16a31cf20ecbddc0678a06ff62af5d1
eduardo-toste <duciochetti@gmail.com>
2025-11-17 17:54:50 -0300

    feat: add method to find Turmas by Professor

--- src/main/java/com/fighthub/repository/TurmaRepository.java
+++ src/main/java/com/fighthub/repository/TurmaRepository.java
@@ -1,6 +1,7 @@
 package com.fighthub.repository;
 
 import com.fighthub.model.Aluno;
+import com.fighthub.model.Professor;
 import com.fighthub.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +11,7 @@ import java.util.UUID;
 public interface TurmaRepository extends JpaRepository<Turma, UUID> {
 
     List<Turma> findAllByAlunos(Aluno aluno);
+
+    List<Turma> findAllByProfessor(Professor professor);
 
 }
