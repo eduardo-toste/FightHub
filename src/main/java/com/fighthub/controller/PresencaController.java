2754577a0c4723706436cac551054ba08d3a7a0c
eduardo-toste <duciochetti@gmail.com>
2025-11-19 12:29:02 -0300

    feat: add endpoint to list attendance by class with pagination support

--- src/main/java/com/fighthub/controller/PresencaController.java
+++ src/main/java/com/fighthub/controller/PresencaController.java
@@ -1,10 +1,13 @@
 package com.fighthub.controller;
 
 import com.fighthub.dto.presenca.PresencaRequest;
+import com.fighthub.dto.presenca.PresencaResponse;
 import com.fighthub.service.PresencaService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -29,4 +32,13 @@ public class PresencaController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @GetMapping("/aulas/{idAula}/presencas")
+    @PreAuthorize("hasAnyRole('ADMIN', 'PROFESSOR')")
+    public ResponseEntity<Page<PresencaResponse>> listarPresencasPorAula(
+            @PathVariable UUID idAula,
+            Pageable pageable,
+            HttpServletRequest httpServletRequest) {
+        var presencas = presencaService.listarPresencasPorAula(idAula, pageable, httpServletRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(presencas);
+    }
 }
