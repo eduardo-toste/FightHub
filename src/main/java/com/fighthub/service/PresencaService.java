cdfef9fe1e162f51a264556e75543cf658a1ad59
eduardo-toste <duciochetti@gmail.com>
2025-11-19 12:29:08 -0300

    feat: add method to list attendance by class with authorization check and pagination

--- src/main/java/com/fighthub/service/PresencaService.java
+++ src/main/java/com/fighthub/service/PresencaService.java
@@ -1,12 +1,17 @@
 package com.fighthub.service;
 
 import com.fighthub.dto.presenca.PresencaRequest;
+import com.fighthub.dto.presenca.PresencaResponse;
 import com.fighthub.exception.*;
+import com.fighthub.mapper.PresencaMapper;
 import com.fighthub.model.*;
 import com.fighthub.model.enums.Role;
+import com.fighthub.model.enums.SubscriptionStatus;
 import com.fighthub.repository.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,6 +62,19 @@ public class PresencaService {
         );
     }
 
+    public Page<PresencaResponse> listarPresencasPorAula(UUID idAula, Pageable pageable, HttpServletRequest httpServletRequest) {
+        Usuario usuarioLogado = obterUsuarioLogado(httpServletRequest);
+        Aula aula = buscarAulaPorId(idAula);
+
+        if (usuarioLogado.getRole() == Role.PROFESSOR && !verificarSeProfessorDaAula(usuarioLogado, aula)) {
+            throw new ValidacaoException("Professor não autorizado a verificar presenças para esta aula.");
+        }
+
+        List<Inscricao> inscricoes = buscarInscricoesPorAulaEStatus(aula, SubscriptionStatus.INSCRITO);
+
+        return PresencaMapper.toPageDTO(presencaRepository.findAllByInscricaoIn(inscricoes, pageable));
+    }
+
     private Optional<Presenca> validarPresencaOperacao(UUID idAula, Inscricao inscricao, Usuario usuarioLogado) {
         Aula aula = buscarAulaPorId(idAula);
         Role role = usuarioLogado.getRole();
@@ -97,6 +115,10 @@ public class PresencaService {
     private Inscricao buscarInscricaoPorId(UUID idInscricao) {
         return inscricaoRepository.findById(idInscricao)
                 .orElseThrow(InscricaoNaoEncontradaException::new);
+    }
+
+    private List<Inscricao> buscarInscricoesPorAulaEStatus(Aula aula, SubscriptionStatus status) {
+        return inscricaoRepository.findAllByAulaAndStatus(aula, status);
     }
 
     private Usuario obterUsuarioLogado(HttpServletRequest request) {
