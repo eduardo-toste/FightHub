837bd5666f88cb17d70b32d80983ce518fdc786f
eduardo-toste <duciochetti@gmail.com>
2025-12-11 15:31:02 -0300

    feat: change totalAlunosAtivos, totalAlunosInativos, and novosAlunosUltimos30Dias types from int to long in GeralDashboardResponse

--- src/main/java/com/fighthub/dto/dashboard/GeralDashboardResponse.java
+++ src/main/java/com/fighthub/dto/dashboard/GeralDashboardResponse.java
@@ -2,11 +2,10 @@ package com.fighthub.dto.dashboard;
 
 public record GeralDashboardResponse(
 
-        int totalAlunosAtivos,
-        int totalAlunosInativos,
-        int novosAlunosUltimos30Dias,
-        int alunosQueSairamUltimos30Dias,
-        double idadeMediaAlunos
+        long totalAlunosAtivos,
+        long totalAlunosInativos,
+        long novosAlunosUltimos30Dias,
+        int idadeMediaAlunos
 
 ) {
 }
