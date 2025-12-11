a645cd416e443d1cdb487a33f66edc0f358d8c76
eduardo-toste <duciochetti@gmail.com>
2025-12-11 15:30:47 -0300

    feat: add endpoint to fetch dashboard data with authorization

--- src/main/java/com/fighthub/controller/DashboardController.java
+++ src/main/java/com/fighthub/controller/DashboardController.java
@@ -1,7 +1,11 @@
 package com.fighthub.controller;
 
+import com.fighthub.dto.dashboard.DashboardResponse;
 import com.fighthub.service.DashboardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,10 @@ public class DashboardController {
 
     private final DashboardService dashboardService;
 
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'COORDENADOR')")
+    public ResponseEntity<DashboardResponse> buscarDadosDashboard() {
+        DashboardResponse dashboardData = dashboardService.getDashboardData();
+        return ResponseEntity.ok(dashboardData);
+    }
 }
