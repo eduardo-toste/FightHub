cb55d8ccf2fdb52ec955744dc12ce2c176684297
eduardo-toste <duciochetti@gmail.com>
2025-12-17 11:27:18 -0300

    feat: add IntegrationExternalException for handling external integration errors

---
+++ src/main/java/com/fighthub/exception/IntegrationExternalException.java
@@ -0,0 +1,9 @@
+package com.fighthub.exception;
+
+public class IntegrationExternalException extends BusinessException {
+
+    public IntegrationExternalException(String message) {
+        super(message);
+    }
+
+}
