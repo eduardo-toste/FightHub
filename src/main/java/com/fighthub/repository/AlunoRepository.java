9b67108946227fe6b6bb7dabfd52f7d5a8c89f6f
eduardo-toste <duciochetti@gmail.com>
2026-01-15 16:22:09 -0300

    feat: add method to find minors without guardians based on birth date

--- src/main/java/com/fighthub/repository/AlunoRepository.java
+++ src/main/java/com/fighthub/repository/AlunoRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,5 +37,15 @@ public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
     @Query(value = "SELECT SUM(date_part('year', age(current_date, a.data_nascimento))) " +
             "FROM alunos a WHERE a.matricula_ativa = :status", nativeQuery = true)
     long sumAgesByMatriculaAtiva(@Param("status") boolean status);
+
+    @Query("""
+            SELECT a FROM Aluno a
+            WHERE a.dataNascimento > :dateLimite
+            AND (a.responsaveis IS EMPTY OR SIZE(a.responsaveis) = 0)
+            AND a.matriculaAtiva = true
+            ORDER BY a.usuario.nome
+            """)
+    @EntityGraph(attributePaths = {"usuario", "responsaveis", "responsaveis.usuario"})
+    List<Aluno> findMenoresSemResponsavel(@Param("dateLimite") LocalDate dateLimite);
 
 }
